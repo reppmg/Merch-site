8178d5de9e8b2c6aeb41ee85076e247f886cf51c
reppmg <reppmg@gmail.com>
2017-06-13 22:38:22 +0300

    Proper registration

--- src/main/java/com/repp/security/SecurityConfiguration.java
+++ src/main/java/com/repp/security/SecurityConfiguration.java
@@ -11,21 +11,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.Filter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by 1 on 17.04.2017.
@@ -41,34 +35,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobalSecurity(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("Maksik").password("fthio").roles("USER");
-        auth.inMemoryAuthentication().withUser("Odmen").password("ftsio").roles("ADMIN", "DBA");
+//        auth.inMemoryAuthentication().withUser("Maksik").password("fthio").roles("USER");
+//        auth.inMemoryAuthentication().withUser("Odmen").password("ftsio").roles("ADMIN", "DBA");
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        final AuthenticationEntryPoint aep = new AuthenticationEntryPoint() {
-
-            @Override
-            public void commence(final HttpServletRequest request,
-                                 final HttpServletResponse response,
-                                 final AuthenticationException authException) throws IOException,
-                    ServletException {
-                response.sendRedirect("/registration.html");
-            }
-        };
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(aep);
-
         http.authorizeRequests()
                 .antMatchers("/", "/good/**", "/login**", "/js/**", "/css/**", "/fonts/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/cup/**", "/shirt/**", "/about.html").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
 
-                .antMatchers(HttpMethod.POST, "/cup/**").access("ROLE_ADMIN")
-                .and().formLogin().successForwardUrl("/registration.html")
+                .antMatchers(HttpMethod.POST, "/cup/**").access("hasRole(ROLE_ADMIN)")
+//                .and().formLogin().successForwardUrl("/registration.html")
                 .and().logout().logoutSuccessUrl("/").permitAll()
 //                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
